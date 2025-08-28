7877ea8a2dc3ceea31a6c45beb34cfd1d7765260
Abhishek Kumar <ak431921@gmail.com>
2025-08-28 20:04:41 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -155,7 +155,7 @@ function App() {
         }}
       >
         <Typography variant="h3" color="white" fontWeight="bold">
-          Funky CRUD app12 🎉
+          Funky CRUD app 🎉
         </Typography>
       </Box>
 
